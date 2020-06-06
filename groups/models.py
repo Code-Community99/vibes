b7364d5060c6e9440001a600af7138dea4a83824
PaymasterMax <duncansantiago18@gmail.com>
2020-06-06 12:24:27 -0700

    patch

--- groups/models.py
+++ groups/models.py
@@ -5,7 +5,7 @@ from signup.models import signup
 
 
 class Groups(models.Model):
-    id = models.AutoField(primary_key = True)
+    id = models.AutoField(primary_key = True , serialize = True)
     group_admin = models.ForeignKey(signup ,on_delete = models.CASCADE)
     group_name = models.CharField(max_length = 30)
     group_icon = models.ImageField()
@@ -20,6 +20,15 @@ class Members(models.Model):
     group_id = models.ManyToManyField(Groups)
     group_member = models.ForeignKey(signup , on_delete = models.CASCADE)
 
-
     class Meta:
         db_table = "Group_members"
+
+class Group_content(models.Model):
+    postuid = models.AutoField(primary_key = True , serialize = True)
+    poster = models. ForeignKey(signup , related_name = "group_content_bridge" , on_delete = models.CASCADE)
+    group_post_own = models.ForeignKey(Groups , related_name = "post_group_own" , on_delete = models.CASCADE)
+    post_content = models.CharField(max_length = 1024)
+    post_time = models.DateTimeField(auto_now_add = True)
+
+    class Meta:
+        db_table = "group_content"
--- groups/views.py
+++ groups/views.py
@@ -2,15 +2,13 @@ from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from discover.models import Followers
 from signup.models import signup
-from .models import Groups as Groupmodel,Members
+from .models import Groups as Groupmodel,Members , Group_content as gc
 from .forms import gform
 from django.db.models import Count
 from django.db.models import Count
-
+import json
 from notifications.models import notif
 # Create your views here.
-
-
 def groupsview(request):
     try:
         request.session['username']
@@ -18,9 +16,7 @@ def groupsview(request):
     except Exception as e:
         return redirect("/login/")
     else:
-        for x in users:
         Group = Groupmodel.objects.order_by("-group_create")
-
         return render(request , "groups/group.html" , context = {"users":users , "Group":Group , "current":request.session['username']})
 
 def creategroup(request):
@@ -30,9 +26,10 @@ def creategroup(request):
         try:
             group_name = request.POST['group_name']
             group_icon = request.FILES['group_icon']
-            group_description = request.POST['Description']
+            group_description = request.POST['group_description']
 
-        except Exception as e:
+        except KeyError as e:
+            print("{}\n\n\n\n".format(e))
             return render(request , "groups/add.html" , context = {"form":form})
         else:
             mem = Members(group_member_id = user_data.uid)
@@ -45,6 +42,7 @@ def creategroup(request):
             return redirect("/groups/")
     else:
         return render(request , "groups/add.html" , context = {"form":form})
+
 def join_group(request , gid):
     try:
         user = signup.objects.get(username = request.session['username'])
@@ -65,9 +63,25 @@ def join_group(request , gid):
             admin = obj1.group_admin_id
             notif.objects.create(source_id = mem.group_member.uid, destination_id = admin,message = user.username + " just joined " + obj1.group_name)
             return redirect("/groups/")
+
 def group_viewer(request , group_name):
-    GroupName = Groupmodel.objects.get(id = group_name).group_name
+    GroupName = Groupmodel.objects.get(id = group_name)
     GroupInfor = Groupmodel.objects.get(id = group_name)
     admin_name = Groupmodel.objects.get(id = group_name).group_admin
     group_member_count = Groupmodel.objects.get(id = group_name).members_set.all().count()
-    return render(request , "groups/onegroup.html" , context = {"GroupName":GroupName , "groupinfor":GroupInfor , "groupadmin":admin_name , "member_count":group_member_count})
+    all_topics = gc.objects.filter(group_post_own_id = group_name)
+    myinfo = signup.objects.get(username = request.session["username"])
+    return render(request , "groups/onegroup.html" , context = {"GroupName":GroupName , "groupinfor":GroupInfor , "myinfo":myinfo, "groupadmin":admin_name , "member_count":group_member_count  , "group_topics":all_topics})
+
+
+def post_to_group(request , gid):
+    if request.method =="POST":
+        try:
+            userinfo = signup.objects.get(username = request.session["username"])
+        except Exception as e:
+            return redirect("/groups/")
+        else:
+            post_obj = gc.objects.create(poster_id = userinfo.uid , group_post_own_id = gid , post_content = request.POST["post_content"])
+            profilepic = ""
+            # post_obj.poster.profilepic
+            return HttpResponse(json.dumps({"response":True, "poster_name":post_obj.poster.username, "responsetext":request.POST["post_content"] ,"poster_img":profilepic}) , content_type = "application/json")
