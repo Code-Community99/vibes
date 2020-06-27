c09ca6d1e229d863c74689c813665f7ff0cc0fef
PaymasterMax <duncansantiago18@gmail.com>
2020-06-27 11:43:25 -0700

    pami's updates

--- signup/static/signup/signup.js
+++ signup/static/signup/signup.js
@@ -3,7 +3,6 @@ document.querySelector("#pass1").addEventListener("keypress" , function(e){
   if (document.getElementById("pass1").value.length<8) {
       document.querySelector("#mainpasswordvalid").setAttribute("style" , "color:#ff0000");
       document.querySelector("#mainpasswordvalid").innerHTML = "* Short password *";
-      // document.querySelector("#submit").setAttribute("disabled" , true);
       validate_data.password = false;
   }
   else {
@@ -16,12 +15,10 @@ document.querySelector("#pass2").addEventListener("change" , function(e){
   if (document.getElementById("pass2").value != document.getElementById("pass1").value) {
       document.querySelector("#passwordvalid").setAttribute("style" , "color:#ff0000");
       document.querySelector("#passwordvalid").innerHTML = "* Password do not match *";
-      // document.querySelector("#submit").setAttribute("disabled" , true);
       validate_data.password = false;
   }
   else {
       document.querySelector("#passwordvalid").setAttribute("style" , "display:none;");
-      // document.querySelector("#submit").setAttribute("disabled" , true);
       validate_data.password = true;
   }
 });
