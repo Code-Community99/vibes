6a44b0cd1a7a74be203986e804294b728bf37f05
PaymasterMax <duncansantiago18@gmail.com>
2020-06-27 12:34:22 -0700

    patch

--- login/mailer.py
+++ login/mailer.py
@@ -1,75 +1,23 @@
-from django.contrib.auth.hashers import check_password,make_password
-from django.shortcuts import render , redirect
-from django.http import HttpResponse
-from signup.models import signup
-import smtplib,string,random
-import validate_email as v
+import smtplib as sm ,validate_email as v,string,random
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
-def login(request):
-    try:
-        request.session["username"]
-    except Exception as e:
-        if request.method == "POST":
-            error_log = list()
-            username = request.POST['username']
-            password = request.POST['password']
-
-            try:
-                userinfo = signup.objects.get(username = username)
-                username = userinfo.username
-
-            except Exception as e:
-                error_log.append(" Wrong credentials")
-
-                return render(request , "login/login.html" , context = {"error_log" : error_log})
-
-            else:
-                if check_password(password , userinfo.password):
-
-                    # add username to the session
-                    request.session["username"] = username
-                    request.session['items'] = list()
-                    request.session["loginstatus"] = True
-
-                    # set the sessions expiry date
-                    request.session.set_expiry(0)
-                    # redirect user to home page
-                    return redirect("/")
-                else:
-                    error_log.append("Wrong credentials")
-                    return render(request , "login/login.html" , context = {"error_log" : error_log})
-
-
-        else:
-            return render(request , "login/login.html")
-    else:
-        return redirect("/")
-
-
 
 def forgotcredetials(request):
-
     bug_hunter = []
-
     if request.method == "POST":
         try:
             lostuser = request.POST['email']
-
         except Exception as e:
             bug_hunter.append("Invalid input")
             return render(request , "login/forgot.html" , context = {"error":bug_hunter})
 
         else:
             try:
-                userdata = signup.objects.get(email = lostuser)
-
-            except signup.DoesNotExist as e:
+                userdata = Signup.objects.get(email = lostuser)
+            except Signup.DoesNotExist as e:
                 bug_hunter.append("Email not registered with us")
                 return render(request , "login/forgot.html" , context = {"error":bug_hunter})
-
             else:
-
                 if v.validate_email(lostuser):
                     hashcode = string.digits + string.ascii_letters + string.digits + string.digits
                     hashcode = "".join([random.choice(hashcode) for value in range(10)])
@@ -99,17 +47,6 @@ def forgotcredetials(request):
                         print("Exiting the mail client program")
                         return render(request , "login/thanks.html")
                 else:
-
                     return render(request , "login/forgot.html" , context = {"error":bug_hunter})
-
     else:
         return render(request , "login/forgot.html" , context = {"error":bug_hunter})
-
-
-
-def logout(request):
-    # clear the session variables
-    request.session.clear()
-    request.session.flush()
-    request.session["loginstatus"] = False
-    return redirect("/")
